54b712f8fd7077f6b7441d464811d39015060edf
AbdalrahmanEldeep <abdo.eslam2021@gmail.com>
2023-06-22 23:35:22 +0300

    UPDATE ROUTES APPEND INSTAED ROUTE

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 export const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <>
+      <div>Profile</div>
+      <Flex gap={"10px"} padding={"30px"}>
+        <Link to={"email"}>Email</Link>
+        <Link to={"them"}>Them</Link>
+      </Flex>
+      <Outlet />
+    </>
+  );
+};
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -5,6 +5,8 @@ import { About } from "../pages/About";
 import { Setting } from "../pages/Setting";
 import { Error } from "../pages/Error";
 import { Navbar } from "../components/Navbar";
+import { Email } from "../components/Email";
+import { Them } from "../components/Them";
 
 export default function Router({ children }) {
   return (
@@ -12,7 +14,11 @@ export default function Router({ children }) {
       <Navbar />
       <Routes>
         <Route path="/" element={children} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile />}>
+          <Route index element={<Email />} />
+          <Route path="email" element={<Email />} />
+          <Route path="them" element={<Them />} />
+        </Route>
         <Route path="/about" element={<About />} />
         <Route path="/setting" element={<Setting />} />
         <Route path="*" element={<Error />} />
